1288cd5ff0f81604ceeafa39fa47a02c70894353
Michiel Vanwymeersch <michiel.vanwymeersch@student.howest.be>
2020-12-16 18:42:56 +0100

    Added time for database

--- ledConnectionAzure.py
+++ ledConnectionAzure.py
@@ -1,7 +1,8 @@
 import RPi.GPIO as GPIO
 import random
 import time
-
+import json
+from datetime import datetime
 from azure.iot.device import IoTHubDeviceClient, Message
 
 CONNECTION_STRING = "HostName=IoTAlarm.azure-devices.net;DeviceId=IotDevice;SharedAccessKey=+Rt4fs6qCSGVTa9Uk/D0oNiEgPBTTO9D8LU/+JZcn2A="
@@ -10,7 +11,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.OUT)
 
 led_state = GPIO.input(23)
-MSG_TXT = '{{"led_state": {led_state}}}'
+MSG_TXT = '{{"led_state": {led_state}, "tijd": {tijd}}}'
 
 def iothub_client_init():
     client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
@@ -22,13 +23,9 @@ def iothub_client_telemetry_sample_run():
         client = iothub_client_init()
         print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )
         while True:
-            msg_txt_formatted = MSG_TXT.format(led_state=led_state)
+            msg_txt_formatted = MSG_TXT.format(led_state=led_state, tijd=json.dumps(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+)
             message = Message(msg_txt_formatted)
-
-            if led_state == 0:
-              message.custom_properties["ledAlert"] = "off"
-            else:
-              message.custom_properties["ledAlert"] = "on"
 
             print( "Sending message: {}".format(message))
             client.send_message(message)
