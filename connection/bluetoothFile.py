59ca4e41b8c28d9244bf061717f996563e091515
mathijs.vanwymeersch <mathijs.vanwymeersch@student.howest.be>
2021-01-05 19:11:02 +0100

    Update bluetoothFile.py

--- connection/bluetoothFile.py
+++ connection/bluetoothFile.py
@@ -4,10 +4,7 @@ import bluetooth
 
 def lookForDevice(name):
 	try:
-		if GPIO.input(12)==GPIO.HIGH:
-			raise InputIsActivated
 		devices = bluetooth.discover_devices(duration=10, lookup_names=1, flush_cache=1)
-		print(GPIO.input(12))
 		for device_address, device_name in devices:
 			if GPIO.input(12)==GPIO.HIGH:
 				break
